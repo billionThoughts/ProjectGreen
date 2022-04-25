db8fc389989403b25a9cdeb64dc7528ada39b07d
Evie Xanthopoulou <it1490@uom.edu.gr>
2022-04-25 13:06:00 +0300

    Fixed StakeFrame

--- src/StakeFrame.java
+++ src/StakeFrame.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -8,10 +10,10 @@ import javax.swing.table.DefaultTableModel;
 
 public class StakeFrame extends JFrame {
 	private UserAccount signedInAccount;
-	private JPanel panel, titlePanel, stakePanel, stakingsPanel;
-	private JLabel titleLabel, stakeLabel, amountLabel, apyLabel;
+	private JPanel panel;
+	private JLabel titleLabel, stakeLabel, amountLabel, apyLabel, backgroundIconLabel;
 	private JTextField amountField;
-	private JButton backButton, stakeButton, unstakeButton;
+	private JButton homeButton, stakeButton, unstakeButton;
 	private JTable stakingsTable;
 	private JScrollPane scrollPane;
 	private Transaction selectedStaking;
@@ -22,13 +24,16 @@ public class StakeFrame extends JFrame {
 		signedInAccount = db.signedInAccountDeserialization();
 		
 		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setLayout(null);
 		
-		//title panel
-		titlePanel = new JPanel();
+		homeButton = new JButton("Home");
+		homeButton.setBackground(new Color(255, 153, 102));
+		homeButton.setBounds(10, 455, 97, 36);
+		panel.add(homeButton);
 		
-		titleLabel = new JLabel("Stake Tokens");
-		backButton = new JButton("Back");
-		backButton.addActionListener(new ActionListener() {
+		//ActionListener for homeButton
+		homeButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -37,16 +42,40 @@ public class StakeFrame extends JFrame {
 			}
 		});
 		
-		titlePanel.add(titleLabel);
-		titlePanel.add(backButton);
+		titleLabel = new JLabel("Stake Tokens");
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		titleLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		titleLabel.setBounds(0, 11, 827, 35);
+		panel.add(titleLabel);
 		
-		//stake panel
-		stakePanel = new JPanel();
 		stakeLabel = new JLabel("Stake your tokens to earn passive rewards");
+		stakeLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		stakeLabel.setBounds(109, 86, 264, 14);
+		panel.add(stakeLabel);
+		
 		amountLabel = new JLabel("AMOUNT TO STAKE");
-		amountField = new JTextField("amount");
+		amountLabel.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		amountLabel.setBounds(109, 140, 117, 14);
+		panel.add(amountLabel);
+		
+		amountField = new JTextField();
+		amountField.setBounds(108, 156, 181, 20);
+		amountField.setColumns(10);
+		panel.add(amountField);
+		
 		apyLabel = new JLabel("12% APY");
-		stakeButton = new JButton("STAKE");
+		apyLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		apyLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		apyLabel.setBounds(299, 159, 78, 14);
+		panel.add(apyLabel);
+		
+		stakeButton = new JButton("Stake");
+		stakeButton.setBackground(new Color(0, 204, 255));
+		stakeButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		stakeButton.setBounds(654, 129, 117, 46);
+		panel.add(stakeButton);
+		
+		//ActionListener for stakeButton
 		stakeButton.addActionListener(new ActionListener() {
 
 			@Override
@@ -59,29 +88,31 @@ public class StakeFrame extends JFrame {
 					db.signedInAccountSerialization(signedInAccount);
 					stakingsTable.setModel(loadData());
 				}
-				else JOptionPane.showMessageDialog(null, "You don't have enough tokens");
+				else JOptionPane.showMessageDialog(null, "You don't have enough tokens",
+						"Staking Error", JOptionPane.ERROR_MESSAGE);
 			}
 		});
 		
-		stakePanel.add(stakeLabel);
-		stakePanel.add(amountLabel);
-		stakePanel.add(amountField);
-		stakePanel.add(apyLabel);
-		stakePanel.add(stakeButton);
-		
-		//stakings panel
-		stakingsPanel = new JPanel();
-		scrollPane = new JScrollPane();
-		//stakingsTable creation
 		stakingsTable = new JTable(this.loadData());
+		
 		//staking selection
 		stakingsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 	    ListSelectionModel listModel = stakingsTable.getSelectionModel();
 	    listModel.addListSelectionListener(new StakingSelectionListener());
+	    
+	    scrollPane = new JScrollPane();
+		scrollPane.getViewport().add(stakingsTable);
+		scrollPane.setPreferredSize(new Dimension(400, 183));
+		scrollPane.setBounds(109, 256, 435, 103);
+		panel.add(scrollPane);
 		
+		unstakeButton = new JButton("Unstake");
+		unstakeButton.setBackground(new Color(0, 204, 255));
+		unstakeButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		unstakeButton.setBounds(654, 274, 117, 46);
+		panel.add(unstakeButton);
 		
-		unstakeButton = new JButton("UNSTAKE");
-		
+		//ActionListener for unstakeButton
 		unstakeButton.addActionListener(new ActionListener() {
 
 			@Override
@@ -94,22 +125,19 @@ public class StakeFrame extends JFrame {
 					JOptionPane.showMessageDialog(null, "Successful unstake, you get " + amount + " tokens!");
 					selectedStaking = null;
 				}
-				else JOptionPane.showMessageDialog(null, "You haven't selected a staking");
+				else JOptionPane.showMessageDialog(null, "You haven't selected a staking",
+						"Staking Warning", JOptionPane.WARNING_MESSAGE);
 			}
 		});
 		
-		scrollPane.getViewport().add(stakingsTable);
-		scrollPane.setPreferredSize(new Dimension(400, 183));
-		
-		stakingsPanel.add(scrollPane);
-		stakingsPanel.add(unstakeButton);
-		
-		panel.add(titlePanel);
-		panel.add(stakePanel);
-		panel.add(stakingsPanel);
+		backgroundIconLabel = new JLabel(new ImageIcon(getClass().getResource("/images/background.jpg")));
+		backgroundIconLabel.setBounds(0, 0, 870, 520);
+		panel.add(backgroundIconLabel);
 		
 		this.setContentPane(panel);
-		this.setSize(800, 550);
+		
+		this.setSize(870, 545);
+		this.setResizable(false);
 		this.setTitle("Stake Screen");
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
@@ -135,7 +163,6 @@ public class StakeFrame extends JFrame {
 				i++;
 			}
 		}
-		
 		return new DefaultTableModel(data, columnNames);
 	}
 	
@@ -170,5 +197,4 @@ public class StakeFrame extends JFrame {
 			 }
 		}
 	}
-
 }
